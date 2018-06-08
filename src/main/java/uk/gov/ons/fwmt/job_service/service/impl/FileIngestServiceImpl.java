9c26f7c4f5b066a592d99179fe10995d9fe5e07e
jacobharrison <jacob.harrison@qa.com>
2018-06-08 13:15:40 +0100

    Continuation of testing FileIngestService. All tests are done - some need more //then statements Combined FilenameParserTest into FileIngestServiceTest due to some duplicated test coverage and cleaned up some unneeded tests FilenameParserTest.class was then deleted because deleting code is fun Cleaned up FileIngestServiceImpl by splitting up the 200 line method because it made me sad to look at and now its a little cleaner and easier to read - probably Current coverage of FileIngestService Class:100% , Method:100% , Lines:84% probably wont get much higher than this now

--- src/main/java/uk/gov/ons/fwmt/job_service/service/impl/FileIngestServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/job_service/service/impl/FileIngestServiceImpl.java
@@ -28,47 +28,66 @@ public class FileIngestServiceImpl implements FileIngestService {
   public static final DateTimeFormatter TIMESTAMP_FORMAT_WINDOWS = DateTimeFormatter
       .ofPattern("yyyy-MM-dd'T'HH-mm-ss'Z'");
 
-  // TODO consider spliting this up into manageable chunks
   protected Filename verifyCSVFilename(String rawFilename, String expectedEndpoint) throws InvalidFileNameException {
     log.info("Began a filename parse for " + rawFilename);
 
-    // // Check extension
-    String[] dotSplit = rawFilename.split("\\.");
-    if (dotSplit.length != 2 || !("csv".equals(dotSplit[1]))) {
-      throw new InvalidFileNameException(rawFilename, "No 'csv' extension");
-    }
+    // Check file extension
+    String[] filenameSplitByDot = checkFileExtension(rawFilename);
 
-    // // Split the filename
-    String[] underscoreSplit = dotSplit[0].split("_");
+    // Split the filename by underscore
+    String[] filenameSplitByUnderscore = filenameSplitByDot[0].split("_");
 
-    // // Extract the endpoint
-    String endpoint = underscoreSplit[0];
+    String endpoint = extractEndpoint(rawFilename, expectedEndpoint, filenameSplitByUnderscore);
+
+    // Detect survey type
+    LegacySampleSurveyType tla = getLegacySampleSurveyType(filenameSplitByUnderscore, endpoint);
+
+    // Timestamp validation
+    String rawTimestamp = filenameSplitByUnderscore[filenameSplitByUnderscore.length - 1];
+    log.debug("File timestamp detected as " + rawTimestamp);
+    LocalDateTime timestamp = getLocalDateTime(rawFilename, rawTimestamp);
+
+    log.info("Passed a filename check");
+
+    return new Filename(endpoint, tla, timestamp);
+  }
+
+  protected String extractEndpoint(String rawFilename, String expectedEndpoint, String[] filenameSplitByUnderscore)
+      throws InvalidFileNameException {
+    String endpoint = filenameSplitByUnderscore[0];
     log.debug("File endpoint detected as " + endpoint);
 
-    // // Check the endpoint against expectations
-    // Ensure that section 'B' matches our endpoint
     if (!expectedEndpoint.equals(endpoint)) {
       throw new InvalidFileNameException(rawFilename, "File had an incorrect endpoint of " + endpoint);
     }
 
     switch (endpoint) {
     case "staff":
-      if (underscoreSplit.length != 2)
+      if (filenameSplitByUnderscore.length != 2)
         throw new InvalidFileNameException(rawFilename, "File names for staff should contain one underscore");
       break;
     case "sample":
-      if (underscoreSplit.length != 3)
+      if (filenameSplitByUnderscore.length != 3)
         throw new InvalidFileNameException(rawFilename, "File names for samples should contain two underscores");
       break;
     default:
       throw new IllegalArgumentException("File had an unrecognized endpoint of " + endpoint);
     }
+    return endpoint;
+  }
 
-    // // Validate the TLA
-    // Only for the "sample" endpoint
+  protected String[] checkFileExtension(String rawFilename) throws InvalidFileNameException {
+    String[] filenameSplitByDot = rawFilename.split("\\.");
+    if (filenameSplitByDot.length != 2 || !("csv".equals(filenameSplitByDot[1]))) {
+      throw new InvalidFileNameException(rawFilename, "No 'csv' extension");
+    }
+    return filenameSplitByDot;
+  }
+
+  protected LegacySampleSurveyType getLegacySampleSurveyType(String[] filenameSplitByUnderscore, String endpoint) {
     LegacySampleSurveyType tla = null;
     if (endpoint.equals("sample")) {
-      String tlaString = underscoreSplit[1];
+      String tlaString = filenameSplitByUnderscore[1];
       log.debug("File TLA detected as " + tlaString);
       switch (tlaString) {
       case "LFS":
@@ -81,12 +100,11 @@ public class FileIngestServiceImpl implements FileIngestService {
         throw new IllegalArgumentException("File had an unrecognized TLA of " + tlaString);
       }
     }
+    return tla;
+  }
 
-    // // Validate the timestamp
-    // The timestamp is always the last part of the underscore-delimited section
-    String rawTimestamp = underscoreSplit[underscoreSplit.length - 1];
+  protected LocalDateTime getLocalDateTime(String rawFilename, String rawTimestamp) throws InvalidFileNameException {
     LocalDateTime timestamp;
-    log.debug("File timestamp detected as " + rawTimestamp);
     try {
       timestamp = LocalDateTime.parse(rawTimestamp, TIMESTAMP_FORMAT_WINDOWS);
     } catch (DateTimeParseException e) {
@@ -100,14 +118,10 @@ public class FileIngestServiceImpl implements FileIngestService {
         throw new InvalidFileNameException(rawFilename, "Invalid timestamp of " + rawTimestamp, e);
       }
     }
-
-    log.info("Passed a filename check");
-
-    return new Filename(endpoint, tla, timestamp);
+    return timestamp;
   }
 
   public FileIngest ingestSampleFile(MultipartFile file) throws IOException, InvalidFileNameException {
-    // check filename
     Filename filename = verifyCSVFilename(file.getOriginalFilename(), "sample");
 
     Reader reader = new InputStreamReader(file.getInputStream());
--- src/test/java/uk/gov/ons/fwmt/job_service/service/impl/FileIngestServiceImplTest.java
+++ src/test/java/uk/gov/ons/fwmt/job_service/service/impl/FileIngestServiceImplTest.java
@@ -1,88 +1,264 @@
 package uk.gov.ons.fwmt.job_service.service.impl;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import uk.gov.ons.fwmt.job_service.data.file_ingest.FileIngest;
+import uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyType;
 import uk.gov.ons.fwmt.job_service.exceptions.types.InvalidFileNameException;
 import uk.gov.ons.fwmt.job_service.exceptions.types.MediaTypeNotSupportedException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyType.GFF;
+import static uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyType.LFS;
 
 public class FileIngestServiceImplTest {
 
   private FileIngestServiceImpl fileIngestService = new FileIngestServiceImpl();
 
+  private final String[] validSampleFileNames = {
+      "sample_GFF_2018-04-24T19:09:54Z.csv",
+      "sample_GFF_2018-04-24T19-09-54Z.csv",
+      "sample_LFS_2018-04-24T19:31:25Z.csv",
+      "sample_LFS_2018-04-24T19-31-25Z.csv",
+  };
+
+  private final String[] validStaffFileNames = {
+      "staff_2016-04-24T19:09:54Z.csv",
+      "staff_2018-04-24T19:31:25Z.csv",
+      "staff_2018-04-24T19-31-25Z.csv",
+  };
+
+  private final String[] invalidSampleFileNames = {
+      "BLAH_BLAH-THIS_IS_WRONG",
+      "sample_2018-04-24T19-31-25Z.csv",
+      "sample_LFS_2018-04-24T19-31-25Z.csp",
+  };
+
+  private final String[] invalidStaffFileNames = {
+      "BLAH_BLAH-THIS_IS_WRONG",
+      "staff_2018-04-24R19-31-25Z.csv",
+      "staff_EG_2018-04-24L19-31-25Z.csv",
+      "foo_LFS_2018-04-24L19-31-25Z.csv",
+      "staff_LFS_2018-04-24T19-31-25Z.csv",
+      "staff_2018-04-24T19:31:25Z.csp",
+  };
+
   @Test
-  public void verifyGFFSampleCSVFilename() throws InvalidFileNameException {
-    //Given
-    String expectedRawFilename = "sample_GFF_2018-04-24T19-09-54Z.csv";
-    String expectedEndpoint = "sample";
-
-    //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
+  public void checkValidSampleFileNames() throws InvalidFileNameException {
+    for (String filename : validSampleFileNames) {
+      assertNotNull(fileIngestService.verifyCSVFilename(filename, "sample"));
+    }
   }
 
   @Test
-  public void verifyLFSSampleCSVFilename() throws InvalidFileNameException {
+  public void checkValidStaffFileNames() throws InvalidFileNameException {
+    for (String filename : validStaffFileNames) {
+      assertNotNull(fileIngestService.verifyCSVFilename(filename, "staff"));
+    }
+  }
+
+  @Test
+  public void checkInvalidSampleFileNames() {
+    for (String filename : invalidSampleFileNames) {
+      try {
+        assertNotNull(fileIngestService.verifyCSVFilename(filename, "sample"));
+        // we should throw an InvalidFileNameException before this point
+        fail("False negative - filename '" + filename + "' should be invalid");
+      } catch (InvalidFileNameException e) {
+        // junk assertion - if we reach this point we have passed
+        assertTrue(true);
+      }
+    }
+  }
+
+  @Test
+  public void checkInvalidStaffFileNames() {
+    for (String filename : invalidStaffFileNames) {
+      try {
+        assertNotNull(fileIngestService.verifyCSVFilename(filename, "staff"));
+        // we should throw an InvalidFileNameException before this point
+        fail("False negative - filename '" + filename + "' should be invalid");
+      } catch (InvalidFileNameException e) {
+        // junk assertion - if we reach this point we have passed
+        assertTrue(true);
+      }
+    }
+  }
+
+  @Test
+  public void extractSampleEndpoint() throws InvalidFileNameException {
     //Given
-    String expectedRawFilename = "sample_LFS_2018-04-24T19-09-54Z.csv";
+    String rawFilename = "sample_GFF_2018-04-24T19:09:54Z.csv";
     String expectedEndpoint = "sample";
+    String[] filenameSplitByUnderscore = {"sample","GFF","2018-04-24T19:09:54Z.csv"};
 
     //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
   }
 
-  @Test(expected=InvalidFileNameException.class)
-  public void invalidFileExtension() throws InvalidFileNameException {
+  @Test
+  public void extractStaffEndpoint() throws InvalidFileNameException {
     //Given
-    String expectedRawFilename = "sample_GFF_2018-04-24T19-09-54Z.txt";
-    String expectedEndpoint = "wrong";
-
-    //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
-  }
-
-  @Test(expected=InvalidFileNameException.class)
-  public void invalidEndpoint() throws InvalidFileNameException {
-    //Given
-    String expectedRawFilename = "sample_GFF_2018-04-24T19-09-54Z.csv";
-    String expectedEndpoint = "wrongEndpoint";
-
-    //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
-  }
-
-  @Test(expected=InvalidFileNameException.class)
-  public void sampleFileOnlyHasOneUnderscore() throws InvalidFileNameException {
-    //Given
-    String expectedRawFilename = "sample_GFF-2018-04-24T19-09-54Z.csv";
-    String expectedEndpoint = "sample";
-
-    //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
-  }
-
-  @Test(expected=InvalidFileNameException.class)
-  public void staffFileHasTwoUnderscores() throws InvalidFileNameException {
-    //Given
-    String expectedRawFilename = "staff_GFF_2018-04-24T19-09-54Z.csv";
+    String rawFilename = "staff_2016-04-24T19:09:54Z.csv";
     String expectedEndpoint = "staff";
+    String[] filenameSplitByUnderscore = {"staff","2018-04-24T19:09:54Z.csv"};
 
     //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
   }
 
-  @Test(expected=IllegalArgumentException.class)
-  public void unrecognisedTLA() throws InvalidFileNameException {
+  @Test(expected = InvalidFileNameException.class)
+  public void wrongEndpointInSampleFilename() throws InvalidFileNameException {
     //Given
-    String expectedRawFilename = "sample_TLA_2018-04-24T19-09-54Z.csv";
+    String rawFilename = "wrong_GFF_2018-04-24T19:09:54Z.csv";
     String expectedEndpoint = "sample";
+    String[] filenameSplitByUnderscore = {"wrong","GFF","2018-04-24T19:09:54Z.csv"};
 
     //When
-    fileIngestService.verifyCSVFilename(expectedRawFilename,expectedEndpoint);
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
   }
-  // TODO timestamp tests
+
+  @Test(expected = IllegalArgumentException.class)
+  public void correctFilenameSentToWrongEndpoint() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "sample_GFF_2018-04-24T19:09:54Z.csv";
+    String expectedEndpoint = "wrong";
+    String[] filenameSplitByUnderscore = {"wrong","GFF","2018-04-24T19:09:54Z.csv"};
+
+    //When
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
+  }
+
+  @Test(expected = InvalidFileNameException.class)
+  public void sampleFilenameFormattedIncorrectly() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "sample_GFF-2018-04-24T19:09:54Z.csv";
+    String expectedEndpoint = "sample";
+    String[] filenameSplitByUnderscore = {"sample","GFF-2018-04-24T19:09:54Z.csv"};
+
+    //When
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
+  }
+
+  @Test(expected = InvalidFileNameException.class)
+  public void staffFilenameFormattedIncorrectly() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "staff__2016-04-24T19:09:54Z.csv";
+    String expectedEndpoint = "staff";
+    String[] filenameSplitByUnderscore = {"staff","","2018-04-24T19:09:54Z.csv"};
+
+    //When
+    String result = fileIngestService.extractEndpoint(rawFilename,expectedEndpoint,filenameSplitByUnderscore);
+
+    //Then
+    assertEquals(expectedEndpoint,result);
+  }
+
+  @Test
+  public void checkCorrectFileExtension() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "sample_GFF_2018-04-24T19:09:54Z.csv";
+    String expectedFileName = "sample_GFF_2018-04-24T19:09:54Z";
+    String expectedExtension = "csv";
+
+    //When
+    String[] result = fileIngestService.checkFileExtension(rawFilename);
+
+    //Then
+    assertEquals(rawFilename,result[0] + "." + result[1]);
+    assertEquals(expectedFileName, result[0]);
+    assertEquals(expectedExtension, result[1]);
+  }
+
+  @Test(expected = InvalidFileNameException.class)
+  public void incorrectFileExtension() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "sample_GFF_2018-04-24T19:09:54Z.jpg";
+    String expectedExtension = "csv";
+
+    String[] result = fileIngestService.checkFileExtension(rawFilename);
+
+    //Then
+    assertNotEquals(expectedExtension,result[1]);
+  }
+
+  @Test
+  public void getLegacySampleSurveyTypeGFF() {
+    //Given
+    String[] filenameSplitByUnderscore = {"sample","GFF","2018-04-24T19:09:54Z.csv"};
+    String endpoint = "sample";
+
+    //When
+    LegacySampleSurveyType result = fileIngestService.getLegacySampleSurveyType(filenameSplitByUnderscore,endpoint);
+
+    //Then
+    assertEquals(GFF,result);
+  }
+
+  @Test
+  public void getLegacySampleSurveyTypeLFS() {
+    //Given
+    String[] filenameSplitByUnderscore = {"sample","LFS","2018-04-24T19:09:54Z.csv"};
+    String endpoint = "sample";
+
+    //When
+    LegacySampleSurveyType result = fileIngestService.getLegacySampleSurveyType(filenameSplitByUnderscore,endpoint);
+
+    //Then
+    assertEquals(LFS,result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void unrecognizedLegacySampleSurveyType() {
+    //Given
+    String[] filenameSplitByUnderscore = {"sample","TLA","2018-04-24T19:09:54Z.csv"};
+    String endpoint = "sample";
+
+    //When
+    LegacySampleSurveyType result = fileIngestService.getLegacySampleSurveyType(filenameSplitByUnderscore,endpoint);
+
+    //Then
+    assertNotEquals(LFS,result);
+    assertNotEquals(GFF,result);
+  }
+
+  @Test
+  public void getLocalDateTime() throws InvalidFileNameException {
+    //Given
+    String rawFilename = "sample_GFF_2018-04-24T19:09:54Z.csv";;
+    String rawTimestamp = "2018-04-24T19:09:54Z";
+
+    //When
+    LocalDateTime result = fileIngestService.getLocalDateTime(rawFilename,rawTimestamp);
+
+    //Then
+
+  }
 
   @Test
   public void ingestSampleFile() throws IOException, InvalidFileNameException, MediaTypeNotSupportedException {
@@ -90,6 +266,11 @@ public class FileIngestServiceImplTest {
     MultipartFile testFile = new MockMultipartFile("sampleFile","sample_GFF_2018-04-24T19-09-54Z.csv","text/csv", (InputStream) null);
 
     //When
-    fileIngestService.ingestSampleFile(testFile);
+    FileIngest result = fileIngestService.ingestSampleFile(testFile);
+
+    //Then
+    Assert.assertEquals("sample", result.getFilename().getEndpoint());
+    Assert.assertEquals(GFF, result.getFilename().getTla());
+    Assert.assertEquals("2018-04-24T19:09:54", result.getFilename().getTimestamp().toString());
   }
 }
