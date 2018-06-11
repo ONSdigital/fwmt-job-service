07753cc77d513f63aa056c7a323d9c0ae7ec8361
jacobharrison <jacob.harrison@qa.com>
2018-06-11 12:01:03 +0100

    Completing testing CSVParsingService cleaned up some of the code that was unneeded Added new custom exception for missing stage/field period

--- src/test/java/uk/gov/ons/fwmt/job_service/integration_tests/legacy_csv/FieldPeriodTest.java
+++ src/test/java/uk/gov/ons/fwmt/job_service/integration_tests/legacy_csv/FieldPeriodTest.java
@@ -7,14 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import uk.gov.ons.fwmt.job_service.exceptions.types.FWMTCommonException;
 import uk.gov.ons.fwmt.job_service.service.impl.CSVParsingServiceImpl;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,73 +22,73 @@ public class FieldPeriodTest {
   @Autowired
   private CSVParsingServiceImpl csvParsingServiceImpl;
   
-  void testGFFDate(LocalDate expected, String fieldPeriod) {
+  void testGFFDate(LocalDate expected, String fieldPeriod) throws FWMTCommonException {
     LocalDate actual = csvParsingServiceImpl.convertToGFFDate(fieldPeriod);
     assertEquals(expected, actual);
   }
 
-  void testLFSDate(LocalDate expected, String fieldPeriod) {
+  void testLFSDate(LocalDate expected, String fieldPeriod) throws FWMTCommonException {
     LocalDate actual = csvParsingServiceImpl.convertToLFSDate(fieldPeriod);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void fieldPeriodToDateGFFforMonthWith31Days() {
+  public void fieldPeriodToDateGFFforMonthWith31Days() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2018, 7, 31), "807");
   }
 
   @Test
-  public void fieldPeriodToDateGFFforMonthWith30Days() {
+  public void fieldPeriodToDateGFFforMonthWith30Days() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2018, 6, 30), "806");
   }
   
   @Test
-  public void fieldPeriodToDateGFFforMonthWith31DaysReissue() {
+  public void fieldPeriodToDateGFFforMonthWith31DaysReissue() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2018, 5, 31), "824");
   }
 
   @Test
-  public void fieldPeriodToDateGFFforMonthWith30DaysReissue() {
+  public void fieldPeriodToDateGFFforMonthWith30DaysReissue() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2018, 6, 30), "825");
   }
   
   @Test
-  public void fieldPeriodToDateGFFforMonthWith31Days19() {
+  public void fieldPeriodToDateGFFforMonthWith31Days19() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2019, 7, 31), "907");
   }
 
   @Test
-  public void fieldPeriodToDateGFFforMonthWith30Days19() {
+  public void fieldPeriodToDateGFFforMonthWith30Days19() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2019, 6, 30), "906");
   }
   
   @Test
-  public void fieldPeriodToDateGFFforMonthWith31DaysReissue19() {
+  public void fieldPeriodToDateGFFforMonthWith31DaysReissue19() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2019, 5, 31), "924");
   }
 
   @Test
-  public void fieldPeriodToDateGFFforMonthWith30DaysReissue19() {
+  public void fieldPeriodToDateGFFforMonthWith30DaysReissue19() throws FWMTCommonException {
     testGFFDate(LocalDate.of(2019, 6, 30), "925");
   }
 
   @Test
-  public void fieldPeriodToDateLFSforQuater4Week11() {
+  public void fieldPeriodToDateLFSforQuater4Week11() throws FWMTCommonException {
     testLFSDate(LocalDate.of(2018, 12, 29), "84K");
   }
 
   @Test
-  public void fieldPeriodToDateLFSforQuater2Week2() {
+  public void fieldPeriodToDateLFSforQuater2Week2() throws FWMTCommonException {
     testLFSDate(LocalDate.of(2018, 4, 28), "82B");
   }
   
   @Test
-  public void fieldPeriodToDateLFSforQuater4Week112019() {
+  public void fieldPeriodToDateLFSforQuater4Week112019() throws FWMTCommonException {
     testLFSDate(LocalDate.of(2019, 12, 28), "94K");
   }
 
   @Test
-  public void fieldPeriodToDateLFSforQuater2Week22019() {
+  public void fieldPeriodToDateLFSforQuater2Week22019() throws FWMTCommonException {
     testLFSDate(LocalDate.of(2019, 4, 27), "92B");
   }
 }
--- src/test/java/uk/gov/ons/fwmt/job_service/service/impl/CSVParsingServiceImplTest.java
+++ src/test/java/uk/gov/ons/fwmt/job_service/service/impl/CSVParsingServiceImplTest.java
@@ -2,12 +2,19 @@ package uk.gov.ons.fwmt.job_service.service.impl;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleGFFDataIngest;
 import uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleIngest;
+import uk.gov.ons.fwmt.job_service.exceptions.ExceptionCode;
+import uk.gov.ons.fwmt.job_service.exceptions.types.FWMTCommonException;
+import uk.gov.ons.fwmt.job_service.rest.FieldPeriodResourceService;
+import uk.gov.ons.fwmt.job_service.rest.dto.FieldPeriodDto;
 import uk.gov.ons.fwmt.job_service.utilities.TestIngestBuilder;
 
 import java.io.File;
@@ -15,9 +22,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.LocalDate;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 import static uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyType.GFF;
 import static uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyType.LFS;
 
@@ -25,6 +37,9 @@ import static uk.gov.ons.fwmt.job_service.data.legacy_ingest.LegacySampleSurveyT
 public class CSVParsingServiceImplTest {
 
   @InjectMocks CSVParsingServiceImpl csvParsingServiceImpl;
+  @Mock FieldPeriodResourceService fieldPeriodResourceService;
+  @Mock FieldPeriodDto fieldPeriodDto;
+  @Rule public ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void setFromCSVColumnAnnotations() throws IOException {
@@ -90,7 +105,6 @@ public class CSVParsingServiceImplTest {
     csvParsingServiceImpl.setFromCSVColumnAnnotations(testIngestData, csvParser.iterator().next(),"GFF");
   }
 
-
   @Test
   public void constructTmGFFJobId() throws IOException {
     //Given
@@ -122,14 +136,80 @@ public class CSVParsingServiceImplTest {
   }
 
   @Test
-  public void convertToGFFDate() {
+  public void convertToGFFDate() throws FWMTCommonException {
+    //Given
+    String stage = "807";
+    int year = 2018;
+    int month = 7;
+    int day = 1;
+    LocalDate date = LocalDate.of(year,month,day);
+    when(fieldPeriodResourceService.findByFieldPeriod(any())).thenReturn(Optional.of(fieldPeriodDto));
+    when(fieldPeriodDto.getEndDate()).thenReturn(date);
+
+    //When
+    LocalDate result = csvParsingServiceImpl.convertToGFFDate(stage);
+
+    //Then
+    assertEquals(date,result);
   }
 
   @Test
-  public void convertToLFSDate() {
+  public void convertToGFFDateShouldThrowExceptionWhenStageMissing() throws FWMTCommonException {
+    //Given
+    when(fieldPeriodResourceService.findByFieldPeriod(any())).thenReturn(Optional.empty());
+    expectedException.expect(FWMTCommonException.class);
+    expectedException.expectMessage(ExceptionCode.FWMT_JOB_SERVICE_0011.toString());
+
+    //When
+    csvParsingServiceImpl.convertToGFFDate(anyString());
   }
 
   @Test
-  public void parseLegacySample() {
+  public void convertToLFSDate() throws FWMTCommonException {
+    //Given
+    String fieldPeriod = "807";
+    int year = 2018;
+    int month = 7;
+    int day = 1;
+    LocalDate date = LocalDate.of(year,month,day);
+    when(fieldPeriodResourceService.findByFieldPeriod(any())).thenReturn(Optional.of(fieldPeriodDto));
+    when(fieldPeriodDto.getEndDate()).thenReturn(date);
+
+    //When
+    LocalDate result = csvParsingServiceImpl.convertToLFSDate(fieldPeriod);
+
+    //Then
+    assertEquals(date,result);
+  }
+
+  @Test
+  public void convertToLFSDateShouldThrowExceptionWhenFieldPeriodMissing() throws FWMTCommonException {
+    //Given
+    when(fieldPeriodResourceService.findByFieldPeriod(any())).thenReturn(Optional.empty());
+    expectedException.expect(FWMTCommonException.class);
+    expectedException.expectMessage(ExceptionCode.FWMT_JOB_SERVICE_0011.toString());
+
+    //When
+    csvParsingServiceImpl.convertToLFSDate(anyString());
+  }
+
+  @Test
+  public void parseGFFLegacySample() throws IOException {
+    //Given
+    File testFile = new File("src/test/resources/sampledata/unit_tests/sample_GFF_2018-05-17T15-34-00Z.csv");
+    Reader reader = new InputStreamReader(new FileInputStream(testFile));
+
+    //When
+    csvParsingServiceImpl.parseLegacySample(reader,GFF);
+  }
+
+  @Test
+  public void parseLFSLegacySample() throws IOException {
+    //Given
+    File testFile = new File("src/test/resources/sampledata/unit_tests/sample_LFS_2018-05-17T15-34-00Z.csv");
+    Reader reader = new InputStreamReader(new FileInputStream(testFile));
+
+    //When
+    csvParsingServiceImpl.parseLegacySample(reader,LFS);
   }
 }
