0f3c93c2018cbd7ccb29ba9729eb487c1e8681bb
Simon <sdiaz1000@gmail.com>
2018-06-25 12:50:28 +0100

    FWMT-309 Created Inbound from TM webservice

--- src/main/java/uk/gov/ons/fwmt/job_service/controller/tm_endpoint/GenericOutgoingWs.java
+++ src/main/java/uk/gov/ons/fwmt/job_service/controller/tm_endpoint/GenericOutgoingWs.java
@@ -1,32 +1,34 @@
 package uk.gov.ons.fwmt.job_service.controller.tm_endpoint;
 
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.ObjectFactory;
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendMessageResponse;
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.WebServiceAdapterOutputRequest;
-import lombok.extern.slf4j.Slf4j;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import javax.xml.bind.JAXBElement;
+import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendMessageResponse;
+import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.WebServiceAdapterOutputRequest;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Endpoint
 public class GenericOutgoingWs {
   private static final String NAMESPACE_URI = "http://schemas.consiliumtechnologies.com/services/mobile/2009/03/messaging";
 
-  private void stub(String messageType) {
-    log.debug("Found message type > " + messageType);
-  }
-
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "request")
   @ResponsePayload
-  public JAXBElement<SendMessageResponse> sendAdapterOutput(
-      @RequestPayload JAXBElement<WebServiceAdapterOutputRequest> completeVisitRequest) {
-    stub("SendAdapterOutput");
-    SendMessageResponse response = new SendMessageResponse();
-    ObjectFactory objectFactory = new ObjectFactory();
-    return objectFactory.createSendMessageResponse(response);
-  }
+  public JAXBElement<SendMessageResponse> request(@RequestPayload JAXBElement<WebServiceAdapterOutputRequest> request){
+    SendMessageResponse smr = new SendMessageResponse();
+    QName qname = new QName("request");
+    JAXBElement<SendMessageResponse>  jaxbElement =new JAXBElement<SendMessageResponse>( qname, SendMessageResponse.class, smr); 
+    SendMessageResponse msg = new SendMessageResponse();
+    WebServiceAdapterOutputRequest value = request.getValue();
+    msg.setId(value.getId());
+    jaxbElement.setValue(msg);
+    log.info("Incoming message from TM received. Id:" + value.getId());
+    return jaxbElement; 
+  }  
 }
