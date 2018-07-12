78ac8f5861d78a2875cee4367c8e57004d888da7
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2018-07-12 16:15:40 +0100

    Added address concat if all 6 address fields are present

--- src/main/java/uk/gov/ons/fwmt/job_service/service/impl/totalmobile/TMJobConverterServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/job_service/service/impl/totalmobile/TMJobConverterServiceImpl.java
@@ -105,9 +105,10 @@ public class TMJobConverterServiceImpl implements TMJobConverterService {
     addAddressLines(addressLines, ingest.getAddressLine2());
     addAddressLines(addressLines, ingest.getAddressLine3());
     addAddressLines(addressLines, ingest.getAddressLine4());
+    addAddressLines(addressLines, ingest.getDistrict());
+    addAddressLines(addressLines, ingest.getPostTown());
+    checkNumberOfAddressLines(addressLines);
 
-    addressLines.add(ingest.getDistrict());
-    addressLines.add(ingest.getPostTown());
     location.getAddressDetail().setPostCode(ingest.getPostcode());
     location.setReference(ingest.getSerNo());
 
@@ -146,6 +147,14 @@ public class TMJobConverterServiceImpl implements TMJobConverterService {
   protected void addAddressLines(List<String> addressLines, String addressLine) {
     if (StringUtils.isNotBlank((addressLine))) {
       addressLines.add(addressLine);
+    }
+  }
+
+  protected void checkNumberOfAddressLines(List<String> addressLines) {
+    if (addressLines.size() == 6 ) {
+      String addressConcat = addressLines.get(2) + " " + addressLines.get(3);
+      addressLines.set(2, addressConcat);
+      addressLines.remove(3);
     }
   }
 
