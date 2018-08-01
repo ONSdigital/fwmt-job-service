54be2591a2e9bfd03f0693817863e590f1bee3ec
ThomasPootQA <thomas.poot@qa.com>
2018-08-01 13:42:17 +0100

    Moved classes related to MDC into their own helper Added MDC logging of the file name

--- src/main/java/uk/gov/ons/fwmt/job_service/service/impl/JobServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/job_service/service/impl/JobServiceImpl.java
@@ -3,6 +3,7 @@ package uk.gov.ons.fwmt.job_service.service.impl;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,9 @@ import uk.gov.ons.fwmt.job_service.rest.client.JobResourceServiceClient;
 import uk.gov.ons.fwmt.job_service.service.FileIngestService;
 import uk.gov.ons.fwmt.job_service.service.JobService;
 import uk.gov.ons.fwmt.job_service.utils.SampleFileUtils;
+
+import static uk.gov.ons.fwmt.job_service.config.MDCHelper.FILENAME_KEY;
+import static uk.gov.ons.fwmt.job_service.config.MDCHelper.FILENAME_PREFIX;
 
 @Slf4j
 @Service
@@ -36,14 +40,21 @@ public class JobServiceImpl implements JobService {
           throws IOException{
     log.debug("Handling file with name '{}'", multipartFile.getOriginalFilename());
     File regularFile = SampleFileUtils.convertToRegularFile(multipartFile);
+
+    // log the file name we're working with
+    MDC.put(FILENAME_KEY, FILENAME_PREFIX + regularFile.getName());
+
     log.info("Validation began");
     SampleSummaryDTO sampleSummaryDTO = fileIngestService.validateSampleFile(regularFile);
     log.info("Validation passed");
+
     log.info("Storing CSV in resource service");
     jobResourceServiceClient.storeCSVFile(regularFile, sampleSummaryDTO.getUnprocessedRows().isEmpty());
     log.info("CSV stored successfully");
+
     jobProcessor.processSampleFile(regularFile); // This is an async call
     log.info("Queued asynchronous call to JobProcessor::processSampleFile");
+
     return sampleSummaryDTO;
   }
 }
