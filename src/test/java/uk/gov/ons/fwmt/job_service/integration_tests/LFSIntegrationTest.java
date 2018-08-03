3e4d0b831523f9e0c476ce4a649fe66a2d2839f4
ThomasPootQA <thomas.poot@qa.com>
2018-08-03 01:00:04 +0100

    Built initial tests, without a means of ensuring that the asynchronous process has completed

---
+++ src/test/java/uk/gov/ons/fwmt/job_service/integration_tests/LFSIntegrationTest.java
@@ -0,0 +1,39 @@
+package uk.gov.ons.fwmt.job_service.integration_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
+public class LFSIntegrationTest {
+  @Autowired TaskExecutor taskExecutor;
+
+  @Test
+  public void lfsIntegrationTest() {
+    RestTemplate restTemplate = new RestTemplate();
+
+    LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+    map.add("file", new ClassPathResource("sampledata/integration_tests/sample_LFS_2018-06-06T06-06-06Z.csv"));
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    byte[] encodedBytes = Base64.getEncoder().encode("user:password".getBytes());
+    headers.add("Authorization", "Basic " + new String(encodedBytes));
+
+    HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+    restTemplate.postForEntity("http://localhost:9091/jobs/samples", requestEntity, Void.class);
+
+  }
+}
