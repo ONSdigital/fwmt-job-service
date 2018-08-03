f0ec939a89bc03ae408ce182ec08f87c97502b92
ThomasPootQA <thomas.poot@qa.com>
2018-08-03 14:46:23 +0100

    Worked on polling for the completion of the worker thread, and of properties directing the integration test to the mock instance

--- src/test/java/uk/gov/ons/fwmt/job_service/integration_tests/GFFIntegrationTest.java
+++ src/test/java/uk/gov/ons/fwmt/job_service/integration_tests/GFFIntegrationTest.java
@@ -2,22 +2,33 @@ package uk.gov.ons.fwmt.job_service.integration_tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Base64;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import static org.junit.Assert.fail;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {"spring.profiles.active=integration"})
 @RunWith(SpringRunner.class)
 public class GFFIntegrationTest {
+  @Autowired
+  @Qualifier("processExecutor")
+  TaskExecutor executor;
+
   @Test
-  public void lfsIntegrationTest() {
+  public void gffIntegrationTest() {
     RestTemplate restTemplate = new RestTemplate();
 
     LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
@@ -30,6 +41,17 @@ public class GFFIntegrationTest {
 
     HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
     restTemplate.postForEntity("http://localhost:9091/jobs/samples", requestEntity, Void.class);
+
+    long startTime = System.currentTimeMillis();
+    long timeout = 10000;
+    while (((ThreadPoolTaskExecutor) executor).getActiveCount() > 0 ||
+        (System.currentTimeMillis() - startTime < timeout)) {
+      // wait
+    }
+
+    if (((ThreadPoolTaskExecutor) executor).getActiveCount() > 0) {
+      fail("The application failed to process the request within " + Long.toString(timeout) + " milliseconds");
+    }
 
   }
 }
