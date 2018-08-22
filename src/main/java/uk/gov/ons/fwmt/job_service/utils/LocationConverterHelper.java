26000ac59ad8a05f2b4ab6058252fd4b26cba384
ThomasPootQA <thomas.poot@qa.com>
2018-08-22 22:31:19 +0100

    Partial convertion into a system that uses ApFloat

--- src/main/java/uk/gov/ons/fwmt/job_service/utils/LocationConverterHelper.java
+++ src/main/java/uk/gov/ons/fwmt/job_service/utils/LocationConverterHelper.java
@@ -17,7 +17,7 @@ import java.math.RoundingMode;
 import org.apfloat.Apfloat;
 import org.apfloat.ApfloatMath;
 
-import static java.lang.Math.pow;
+import static org.apfloat.ApfloatMath.*;
 
 public class LocationConverterHelper {
   protected final static EllipsoidParameters AIRY1830_ELLIPSOID =
@@ -30,7 +30,7 @@ public class LocationConverterHelper {
   protected final static DatumParameters WGS84_DATUM = new DatumParameters(WGS84_ELLIPSOID,
       0, 0, 0, 0, 0, 0, 0);
 
-  protected final static double NAT_GRID_SCALE_FACTOR = 0.9996012717;
+  protected final static Apfloat NAT_GRID_SCALE_FACTOR = new Apfloat(0.9996012717);
   protected final static CartesianCoordinate NAT_GRID_ORIGIN_CARTESIAN = new CartesianCoordinate(-100000, 400000, 0);
   protected final static PolarCoordinate NAT_GRID_ORIGIN_POLAR =
       new PolarCoordinate(WGS84_DATUM, toRadians(49), toRadians(-2), 0);
@@ -48,81 +48,91 @@ public class LocationConverterHelper {
     return input * (Math.PI / 180);
   }
 
+  protected static Apfloat toRadians(Apfloat input) {
+    return input.multiply(ApfloatMath.pi(20).divide(new Apfloat(180)));
+  }
+
   protected static double toDegrees(double input) {
     return input * (180 / Math.PI);
   }
 
-  protected static CartesianCoordinate toCartesian(PolarCoordinate input) {
-    double phi = toRadians(input.phi);
-    double lambda = toRadians(input.lambda);
-    double h = 0; // height above ellipsoid - not currently used
-    double a = input.datum.ellipsoid.a;
-    double f = input.datum.ellipsoid.f;
+  protected static Apfloat toDegrees(Apfloat input) {
+    return input.multiply(new Apfloat(180).divide(ApfloatMath.pi(20)));
+  }
 
-    double sin_phi = Math.sin(phi);
-    double cos_phi = Math.cos(phi);
-    double sin_lambda = Math.sin(lambda);
-    double cos_lambda = Math.cos(lambda);
+  protected static CartesianCoordinate toCartesian(PolarCoordinate input) {
+    Apfloat phi = toRadians(input.phi);
+    Apfloat lambda = toRadians(input.lambda);
+    Apfloat h = new Apfloat(0); // height above ellipsoid - not currently used
+    Apfloat a = input.datum.ellipsoid.a;
+    Apfloat f = input.datum.ellipsoid.f;
+
+    Apfloat sin_phi = sin(phi);
+    Apfloat cos_phi = cos(phi);
+    Apfloat sin_lambda = sin(lambda);
+    Apfloat cos_lambda = cos(lambda);
 
     // eSq: 1st eccentricity squared ≡ (a²-b²)/a²
-    double eSq = 2 * f - pow(f, 2);
+    Apfloat eSq = (new Apfloat(2).multiply(f)).subtract(pow(f, 2));
     // nu: radius of curvature in prime vertical
-    double nu = a / Math.sqrt(1 - eSq * sin_phi * sin_phi);
+    Apfloat nu = a.divide(sqrt(new Apfloat(1).subtract(eSq.multiply(pow(sin_phi, 2)))));
 
-    double x = (nu + h) * cos_phi * cos_lambda;
-    double y = (nu + h) * cos_phi * sin_lambda;
-    double z = (nu * (1 - eSq) + h) * sin_phi;
+    Apfloat x = (nu.add(h)).multiply(cos_phi).multiply(cos_lambda);
+    Apfloat y = (nu.add(h)).multiply(cos_phi).multiply(sin_lambda);
+    Apfloat z = (nu.multiply(new Apfloat(1).subtract(eSq)).add(h)).multiply(sin_phi);
 
     return new CartesianCoordinate(x, y, z);
   }
 
   protected static PolarCoordinate toLatLong(CartesianCoordinate input, DatumParameters datum) {
-    double x = input.x;
-    double y = input.y;
-    double z = input.z;
-    double a = datum.ellipsoid.a;
-    double b = datum.ellipsoid.b;
-    double f = datum.ellipsoid.f;
+    Apfloat x = input.x;
+    Apfloat y = input.y;
+    Apfloat z = input.z;
+    Apfloat a = datum.ellipsoid.a;
+    Apfloat b = datum.ellipsoid.b;
+    Apfloat f = datum.ellipsoid.f;
 
-    double e2 = 2 * f - pow(f, 2);   // 1st eccentricity squared ≡ (a²-b²)/a²
-    double epsilon2 = e2 / (1 - e2); // 2nd eccentricity squared ≡ (a²-b²)/b²
-    double p = Math.sqrt(pow(x, 2) + pow(y, 2)); // distance from minor axis
-    double R = Math.sqrt(pow(p, 2) + pow(z, 2)); // polar radius
+    Apfloat e2 = new Apfloat(2).multiply(f).subtract(pow(f, 2));   // 1st eccentricity squared ≡ (a²-b²)/a²
+    Apfloat epsilon2 = e2.divide(new Apfloat(1).subtract(e2)); // 2nd eccentricity squared ≡ (a²-b²)/b²
+    Apfloat p = sqrt(pow(x, 2).add(pow(y, 2))); // distance from minor axis
+    Apfloat R = sqrt(pow(p, 2).add(pow(z, 2))); // polar radius
 
     // parametric latitude (Bowring eqn 17, replacing tanβ = z·a / p·b)
-    double tan_beta = (b * z) / (a * p) * (1 + epsilon2 * b / R);
-    double sin_beta = tan_beta / Math.sqrt(1 + tan_beta * tan_beta);
-    double cos_beta = sin_beta / tan_beta;
+    Apfloat tan_beta = (b.multiply(z)).divide(a.multiply(p))
+        .multiply(new Apfloat(1).add(epsilon2.multiply(b).divide(R)));
+    Apfloat sin_beta = tan_beta.divide(sqrt(new Apfloat(1).add(pow(tan_beta, 2))));
+    Apfloat cos_beta = sin_beta.divide(tan_beta);
 
     // geodetic latitude (Bowring eqn 18: tanφ = z+ε²bsin³β / p−e²cos³β)
-    double phi = Double.isNaN(cos_beta) ?
-        0 :
-        Math.atan2(z + epsilon2 * b * pow(sin_beta, 3), p - e2 * a * pow(cos_beta, 3));
+    Apfloat phi = (cos_beta == null) ?
+        new Apfloat(0) :
+        atan2(z.add(epsilon2.multiply(b).multiply(pow(sin_beta, 3))),
+            p.subtract(e2.multiply(a).multiply(pow(cos_beta, 3))));
 
     // longitude
-    double lambda = Math.atan2(y, x);
+    Apfloat lambda = atan2(y, x);
 
     // height above ellipsoid (Bowring eqn 7) [not currently used]
     // nu: length of the normal terminated by the minor axis
-    double nu = a / Math.sqrt(1 - e2 * pow(Math.sin(phi), 2));
-    double h = p * Math.cos(phi) + z * Math.sin(phi) - (pow(a, 2) / nu);
+    //    Apfloat nu = a / sqrt(1 - e2 * pow(sin(phi), 2));
+    //    Apfloat h = p * cos(phi) + z * sin(phi) - (pow(a, 2) / nu);
 
-    return new PolarCoordinate(datum, toDegrees(phi), toDegrees(lambda), 0);
+    return new PolarCoordinate(datum, toDegrees(phi), toDegrees(lambda), new Apfloat(0));
   }
 
   protected static CartesianCoordinate helmertTransform(CartesianCoordinate input, DatumParameters datum) {
     // normalise parts-per-million to (s+1)
-    double scale = datum.scale / 1e6 + 1;
+    Apfloat scale = datum.scale.divide(new Apfloat(1e6)).add(new Apfloat(1));
 
     // normalize arcseconds to radians
-    double rx = toRadians(datum.rx / 3600);
-    double ry = toRadians(datum.ry / 3600);
-    double rz = toRadians(datum.rz / 3600);
+    Apfloat rx = toRadians(datum.rx.divide(new Apfloat(3600)));
+    Apfloat ry = toRadians(datum.ry.divide(new Apfloat(3600)));
+    Apfloat rz = toRadians(datum.rz.divide(new Apfloat(3600)));
 
     // apply the transformation
-    double x_prime = datum.tx + input.x * scale - input.y * rz + input.z * ry;
-    double y_prime = datum.ty + input.x * rz + input.y * scale - input.z * rx;
-    double z_prime = datum.tz - input.x * ry + input.y * rx + input.z * scale;
+    Apfloat x_prime = datum.tx.add(input.x.multiply(scale)).subtract(input.y.multiply(rz)).add(input.z.multiply(ry));
+    Apfloat y_prime = datum.ty.add(input.x.multiply(rz)).add(input.y.multiply(scale)).subtract(input.z.multiply(rx));
+    Apfloat z_prime = datum.tz.subtract(input.x.multiply(ry)).add(input.y.multiply(rx)).add(input.z.multiply(scale));
 
     return new CartesianCoordinate(x_prime, y_prime, z_prime);
   }
@@ -151,148 +161,167 @@ public class LocationConverterHelper {
   protected static CartesianCoordinate latLongToOsGridRef(PolarCoordinate input) {
     PolarCoordinate translated = convertLatLong(input, OSGB36_DATUM);
 
-    double phi = toRadians(translated.phi);
-    double lambda = toRadians(translated.lambda);
+    Apfloat phi = toRadians(translated.phi);
+    Apfloat lambda = toRadians(translated.lambda);
+
+    // constants
+    Apfloat f1 = new Apfloat(1);
+    Apfloat f1 = new Apfloat(3);
+    Apfloat f5_4 = new Apfloat(5.0).divide(new Apfloat(4.0));
 
     // elipsoid parameters
-    double a = AIRY1830_ELLIPSOID.a;
-    double b = AIRY1830_ELLIPSOID.b;
+    Apfloat a = AIRY1830_ELLIPSOID.a;
+    Apfloat b = AIRY1830_ELLIPSOID.b;
 
-    double F0 = NAT_GRID_SCALE_FACTOR;
+    Apfloat F0 = NAT_GRID_SCALE_FACTOR;
 
     // origin
-    double phi_0 = NAT_GRID_ORIGIN_POLAR.phi;
-    double lambda_0 = NAT_GRID_ORIGIN_POLAR.lambda;
-    double northings_origin = NAT_GRID_ORIGIN_CARTESIAN.x;
-    double eastings_origin = NAT_GRID_ORIGIN_CARTESIAN.y;
+    Apfloat phi_0 = NAT_GRID_ORIGIN_POLAR.phi;
+    Apfloat lambda_0 = NAT_GRID_ORIGIN_POLAR.lambda;
+    Apfloat northings_origin = NAT_GRID_ORIGIN_CARTESIAN.x;
+    Apfloat eastings_origin = NAT_GRID_ORIGIN_CARTESIAN.y;
 
     // e2: eccentricity squared
-    double e2 = 1 - pow(b, 2) / pow(a, 2);
-    double n = (a - b) / (a + b);
+    Apfloat e2 = f1.subtract(pow(b, 2).divide(pow(a, 2)));
+    Apfloat n = (a.subtract(b)).divide(a.add(b));
 
-    double cos_phi = Math.cos(phi);
-    double sin_phi = Math.sin(phi);
-    double tan_phi = Math.tan(phi);
+    Apfloat cos_phi = cos(phi);
+    Apfloat sin_phi = sin(phi);
+    Apfloat tan_phi = tan(phi);
 
-    double v = a * F0 / Math.sqrt(1 - e2 * pow(sin_phi, 2));
-    double rho = a * F0 * (1 - e2) / pow(1 - e2 * pow(sin_phi, 2), 1.5);
-    double eta_2 = v / rho - 1;
+    Apfloat v = a.multiply(F0).divide(sqrt(f1.subtract(e2.multiply(pow(sin_phi, 2)))));
+    Apfloat rho = a.multiply(F0).multiply(f1.subtract(e2))
+        .divide(pow(f1.subtract(e2.multiply(pow(sin_phi, 2))), new Apfloat(1.5)));
+    Apfloat eta_2 = v.divide(rho).subtract(f1);
 
-    double Ma = (1 + n + (5.0 / 4.0) * pow(n, 2) + (5.0 / 4.0) * pow(n, 3)) * (phi - phi_0);
-    double Mb = (3 * n + 3 * n * n + (21.0 / 8.0) * pow(n, 3)) * Math.sin(phi - phi_0) * Math.cos(phi + phi_0);
-    double Mc =
-        ((15.0 / 8.0) * pow(n, 2) + (15.0 / 8.0) * pow(n, 3)) * Math.sin(2 * (phi - phi_0)) * Math.cos(2 * (phi + phi_0));
-    double Md = (35.0 / 24.0) * pow(n, 3) * Math.sin(3 * (phi - phi_0)) * Math.cos(3 * (phi + phi_0));
-    double M = b * F0 * (Ma - Mb + Mc - Md);
+    Apfloat Ma = (f1.add(n).add(f5_4.multiply(pow(n, 2))).add(f5_4.multiply(pow(n, 3)))).multiply(phi.subtract(phi_0));
+    Apfloat Mb = (3 * n + 3 * n * n + (21.0 / 8.0) * pow(n, 3)) * sin(phi.subtract(phi_0)) * cos(phi.add(phi_0));
+    Apfloat Mc =
+        ((15.0 / 8.0) * pow(n, 2) + (15.0 / 8.0) * pow(n, 3)) * sin(2 * (phi - phi_0)) * cos(2 * (phi + phi_0));
+    Apfloat Md = (35.0 / 24.0) * pow(n, 3) * sin(3 * (phi - phi_0)) * cos(3 * (phi + phi_0));
+    Apfloat M = b * F0 * (Ma - Mb + Mc - Md);
 
-    double I = M + northings_origin;
-    double II = (v / 2.0) * sin_phi * cos_phi;
-    double III = (v / 24.0) * sin_phi * pow(cos_phi, 3) * (5 - pow(tan_phi, 2) + 9 * eta_2);
-    double IIIA = (v / 720.0) * sin_phi * pow(cos_phi, 5) * (61 - 58 * pow(tan_phi, 2) + pow(tan_phi, 4));
-    double IV = v * cos_phi;
-    double V = (v / 6.0) * pow(cos_phi, 3) * (v / rho - pow(tan_phi, 2));
-    double VI =
+    Apfloat I = M + northings_origin;
+    Apfloat II = (v / 2.0) * sin_phi * cos_phi;
+    Apfloat III = (v / 24.0) * sin_phi * pow(cos_phi, 3) * (5 - pow(tan_phi, 2) + 9 * eta_2);
+    Apfloat IIIA = (v / 720.0) * sin_phi * pow(cos_phi, 5) * (61 - 58 * pow(tan_phi, 2) + pow(tan_phi, 4));
+    Apfloat IV = v * cos_phi;
+    Apfloat V = (v / 6.0) * pow(cos_phi, 3) * (v / rho - pow(tan_phi, 2));
+    Apfloat VI =
         (v / 120.0) * pow(cos_phi, 5) * (5 - 18 * pow(tan_phi, 2) + pow(tan_phi, 4) + 14 * eta_2 - 58 * pow(tan_phi, 2)
             * eta_2);
 
-    double delta_lambda = lambda - lambda_0;
+    Apfloat delta_lambda = lambda - lambda_0;
 
-    double northings = I + II * pow(delta_lambda, 2) + III * pow(delta_lambda, 4) + IIIA * pow(delta_lambda, 6);
-    double eastings = eastings_origin + IV * delta_lambda + V * pow(delta_lambda, 3) + VI * pow(delta_lambda, 5);
+    Apfloat northings = I + II * pow(delta_lambda, 2) + III * pow(delta_lambda, 4) + IIIA * pow(delta_lambda, 6);
+    Apfloat eastings = eastings_origin + IV * delta_lambda + V * pow(delta_lambda, 3) + VI * pow(delta_lambda, 5);
 
     northings = round(northings, 3);
     eastings = round(eastings, 3);
 
-    return new CartesianCoordinate(eastings, northings, 0);
+    return new CartesianCoordinate(eastings, northings, new Apfloat(0));
   }
 
   protected static PolarCoordinate osGridRefToLatLong(CartesianCoordinate input) {
-    double eastings = input.x;
-    double northings = input.y;
+    Apfloat eastings = input.x;
+    Apfloat northings = input.y;
 
     // elipsoid parameters
-    double a = AIRY1830_ELLIPSOID.a;
-    double b = AIRY1830_ELLIPSOID.b;
+    Apfloat a = AIRY1830_ELLIPSOID.a;
+    Apfloat b = AIRY1830_ELLIPSOID.b;
 
-    double F0 = NAT_GRID_SCALE_FACTOR;
+    Apfloat F0 = NAT_GRID_SCALE_FACTOR;
 
     // origin
-    double phi_0 = NAT_GRID_ORIGIN_POLAR.phi;
-    double lambda_0 = NAT_GRID_ORIGIN_POLAR.lambda;
-    double northings_origin = NAT_GRID_ORIGIN_CARTESIAN.x;
-    double eastings_origin = NAT_GRID_ORIGIN_CARTESIAN.y;
+    Apfloat phi_0 = NAT_GRID_ORIGIN_POLAR.phi;
+    Apfloat lambda_0 = NAT_GRID_ORIGIN_POLAR.lambda;
+    Apfloat northings_origin = NAT_GRID_ORIGIN_CARTESIAN.x;
+    Apfloat eastings_origin = NAT_GRID_ORIGIN_CARTESIAN.y;
 
     // e2: eccintricity squared
-    double e2 = 1 - pow(b, 2) / pow(a, 2);
-    double n = (a - b) / (a + b);
+    Apfloat e2 = new Apfloat(1).subtract(pow(b, 2).divide(pow(a, 2)));
+    Apfloat n = (a.subtract(b)).divide(a.add(b));
 
-    double phi = phi_0;
-    double M = 0;
+    Apfloat phi = phi_0;
+    Apfloat M = new Apfloat(0);
     do {
-      phi = (northings - northings_origin - M) / (a * F0) + phi;
+      phi = (northings.subtract(northings_origin).subtract(M)).divide(a.multiply(F0)).add(phi);
 
-      double Ma = (1 + n + (5.0 / 4.0) * pow(n, 2) + (5.0 / 4.0) * pow(n, 3)) * (phi - phi_0);
-      double Mb = (3 * n + 3 * n * n + (21.0 / 8.0) * pow(n, 3)) * Math.sin(phi - phi_0) * Math.cos(phi + phi_0);
-      double Mc =
-          ((15.0 / 8.0) * pow(n, 2) + (15.0 / 8.0) * pow(n, 3)) * Math.sin(2 * (phi - phi_0)) * Math.cos(2 * (phi + phi_0));
-      double Md = (35.0 / 24.0) * pow(n, 3) * Math.sin(3 * (phi - phi_0)) * Math.cos(3 * (phi + phi_0));
+      Apfloat Ma = (1 + n + (5.0 / 4.0) * pow(n, 2) + (5.0 / 4.0) * pow(n, 3)) * (phi - phi_0);
+      Apfloat Mb = (3 * n + 3 * n * n + (21.0 / 8.0) * pow(n, 3)) * sin(phi - phi_0) * cos(phi + phi_0);
+      Apfloat Mc =
+          ((15.0 / 8.0) * pow(n, 2) + (15.0 / 8.0) * pow(n, 3)) * sin(2 * (phi - phi_0)) * cos(2 * (phi + phi_0));
+      Apfloat Md = (35.0 / 24.0) * pow(n, 3) * sin(3 * (phi - phi_0)) * cos(3 * (phi + phi_0));
       M = b * F0 * (Ma - Mb + Mc - Md);              // meridional arc
     } while (northings - northings_origin - M >= 0.00001);  // ie until < 0.01mm
 
-    double sin_phi = Math.sin(phi);
+    Apfloat sin_phi = sin(phi);
     // nu: transverse radius of curvature
-    double nu = a * F0 / Math.sqrt(1 - e2 * pow(sin_phi, 2));
+    Apfloat nu = a * F0 / sqrt(1 - e2 * pow(sin_phi, 2));
     // rho = meridional radius of curvature
-    double rho = a * F0 * (1 - e2) / Math.pow(1 - e2 * pow(sin_phi, 2), 1.5);
-    double eta_2 = nu / rho - 1;
+    Apfloat rho = a * F0 * (1 - e2) / pow(1 - e2 * pow(sin_phi, 2), 1.5);
+    Apfloat eta_2 = nu / rho - 1;
 
-//    double tan_phi = Math.tan(phi);
-    double tan_phi = ApfloatMath.tan(new Apfloat(phi)).doubleValue();
-    double sec_phi = 1 / Math.cos(phi);
-    double VII = tan_phi / (2 * rho * nu);
-    double VIII = tan_phi / (24 * rho * pow(nu, 3)) * (5 + 3 * pow(tan_phi, 2) + eta_2 - 9 * pow(tan_phi, 2) * eta_2);
-    double IX = tan_phi / (720 * rho * pow(nu, 5)) * (61 + 90 * pow(tan_phi, 2) + 45 * pow(tan_phi, 4));
-    double X = sec_phi / nu;
-    double XI = sec_phi / (6 * pow(nu, 3)) * (nu / rho + 2 * pow(tan_phi, 2));
-    double XII = sec_phi / (120 * pow(nu, 5)) * (5 + 28 * pow(tan_phi, 2) + 24 * pow(tan_phi, 4));
-    double XIIA =
+    //    Apfloat tan_phi = tan(phi);
+    Apfloat tan_phi = tan(new Apfloat(phi)).doubleValue();
+    Apfloat sec_phi = 1 / cos(phi);
+    Apfloat VII = tan_phi / (2 * rho * nu);
+    Apfloat VIII = tan_phi / (24 * rho * pow(nu, 3)) * (5 + 3 * pow(tan_phi, 2) + eta_2 - 9 * pow(tan_phi, 2) * eta_2);
+    Apfloat IX = tan_phi / (720 * rho * pow(nu, 5)) * (61 + 90 * pow(tan_phi, 2) + 45 * pow(tan_phi, 4));
+    Apfloat X = sec_phi / nu;
+    Apfloat XI = sec_phi / (6 * pow(nu, 3)) * (nu / rho + 2 * pow(tan_phi, 2));
+    Apfloat XII = sec_phi / (120 * pow(nu, 5)) * (5 + 28 * pow(tan_phi, 2) + 24 * pow(tan_phi, 4));
+    Apfloat XIIA =
         sec_phi / (5040 * pow(nu, 7)) * (61 + 662 * pow(tan_phi, 2) + 1320 * pow(tan_phi, 4) + 720 * pow(tan_phi, 6));
 
-    double dE = (eastings - eastings_origin);
+    Apfloat dE = (eastings - eastings_origin);
     phi = phi - VII * pow(dE, 2) + VIII * pow(dE, 4) - IX * pow(dE, 6);
-    double lambda = lambda_0 + X * dE - XI * pow(dE, 3) + XII * pow(dE, 5) - XIIA * pow(dE, 7);
+    Apfloat lambda = lambda_0 + X * dE - XI * pow(dE, 3) + XII * pow(dE, 5) - XIIA * pow(dE, 7);
 
-    return new PolarCoordinate(OSGB36_DATUM, toDegrees(phi), toDegrees(lambda), 0);
+    return new PolarCoordinate(OSGB36_DATUM, toDegrees(phi), toDegrees(lambda), new Apfloat(0));
   }
 
   @Data
   @AllArgsConstructor
   public static class CartesianCoordinate {
-    double x, y, z;
+    Apfloat x, y, z;
+
+    CartesianCoordinate(double x, double y, double z) {
+      this(new Apfloat(x), new Apfloat(y), new Apfloat(z));
+    }
   }
 
   @Data
   @AllArgsConstructor
   public static class PolarCoordinate {
     DatumParameters datum;
-    double phi, lambda, h;
+    Apfloat phi, lambda, h;
+
+    PolarCoordinate(DatumParameters datum, double phi, double lambda, double h) {
+      this(datum, new Apfloat(phi), new Apfloat(lambda), new Apfloat(h));
+    }
   }
 
   @Data
   @AllArgsConstructor
   public static class EllipsoidParameters {
-    double a, b, f;
+    Apfloat a, b, f;
+
+    EllipsoidParameters(double a, double b, double f) {
+      this(new Apfloat(a), new Apfloat(b), new Apfloat(f));
+    }
   }
 
   @Data
   @ToString(exclude = "inverse")
   private static class DatumParameters {
     EllipsoidParameters ellipsoid;
-    double tx, ty, tz, scale, rx, ry, rz;
+    Apfloat tx, ty, tz, scale, rx, ry, rz;
     DatumParameters inverse;
 
     private DatumParameters(EllipsoidParameters ellipsoid,
-        double tx, double ty, double tz, double scale, double rx, double ry, double rz, DatumParameters inverse) {
+        Apfloat tx, Apfloat ty, Apfloat tz, Apfloat scale, Apfloat rx, Apfloat ry, Apfloat rz,
+        DatumParameters inverse) {
       this.ellipsoid = ellipsoid;
       this.tx = tx;
       this.ty = ty;
@@ -305,11 +334,17 @@ public class LocationConverterHelper {
     }
 
     public DatumParameters(EllipsoidParameters ellipsoid,
-        double tx, double ty, double tz, double scale, double rx, double ry, double rz) {
+        Apfloat tx, Apfloat ty, Apfloat tz, Apfloat scale, Apfloat rx, Apfloat ry, Apfloat rz) {
       this(ellipsoid, tx, ty, tz, scale, rx, ry, rz, null);
-      this.inverse = new DatumParameters(ellipsoid, -tx, -ty, -tz, -scale, -rx, -ry, -rz, this);
+      this.inverse = new DatumParameters(ellipsoid, tx.negate(), ty.negate(), tz.negate(), scale.negate(), rx.negate(),
+          ry.negate(), rz.negate(), this);
     }
 
+    public DatumParameters(EllipsoidParameters ellipsoid,
+        double tx, double ty, double tz, double scale, double rx, double ry, double rz) {
+      this(ellipsoid, new Apfloat(tx), new Apfloat(ty), new Apfloat(tz), new Apfloat(scale), new Apfloat(rx),
+          new Apfloat(ry), new Apfloat(rz));
+    }
   }
 
 }
